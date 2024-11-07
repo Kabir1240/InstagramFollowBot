a9a073b071e030fadf65c2f9b9fad973edad6297
Kabir Kashif <kashif.kabir60@gmail.com>
2024-11-07 01:44:15 -0800

    added documentation + qol changes

--- insta_follower.py
+++ insta_follower.py
@@ -14,8 +14,6 @@ USER_DATA_DIR = os.environ.get("USER_DATA_DIR")
 # Instagram details
 INSTAGRAM_EMAIL = os.environ.get("INSTAGRAM_EMAIL")
 INSTAGRAM_PASSWORD = os.environ.get("INSTAGRAM_PASSWORD")
-# link to account
-ACCOUNT_TO_FOLLOW = os.environ.get("ACCOUNT_TO_FOLLOW")
 
 # sleep timers
 LOGIN_SLEEP = 3
@@ -25,7 +23,12 @@ FOLLOW_SLEEP = 2
 
 
 class InstaFollower:
-    def __init__(self):
+    def __init__(self) -> None:
+        """
+        initializes selenium with the users settings in incognito mode
+        """
+        
+        # you can comment out all the chrome options code if you dont want to use personal settings
         chrome_options = webdriver.ChromeOptions()
         # keeps browser open even if program closes
         chrome_options.add_experimental_option("detach", True)
@@ -34,11 +37,16 @@ class InstaFollower:
         # launches browser in incognito
         chrome_options.add_argument("--incognito")
         
+        # create driver for chrome
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.get("https://www.instagram.com/")
         
     
-    def login(self):
+    def login(self) -> None:
+        """
+        opens instagram and logs in using your instagram credentials
+        """
+        
         time.sleep(LOGIN_SLEEP)
         input_fields = self.driver.find_elements(By.TAG_NAME, "input")
         email_field = input_fields[0]
@@ -47,15 +55,26 @@ class InstaFollower:
         email_field.send_keys(INSTAGRAM_EMAIL)
         password_field.send_keys(INSTAGRAM_PASSWORD, Keys.ENTER)
     
-    def find_followers(self):
+    def find_followers(self, link) -> None:
+        """
+        uses the link to the account given and opens up followers. Calls follow function after
+        """
+        
         time.sleep(FIND_FOLLOWERS_SLEEP)
-        self.driver.get(ACCOUNT_TO_FOLLOW)
+        self.driver.get(link)
         
         time.sleep(REFRESH_SLEEP)
         followers = self.driver.find_element(By.CSS_SELECTOR, "[href='/mmafighting/followers/']")
         followers.click()
+        
+        self.follow()
     
-    def follow(self):
+    def follow(self) -> None:
+        """
+        scrolls through list of followers and follows everyone. Added a timer between each follow to make it seem more human
+        disclaimer: Instagram may limit your account after this
+        """
+        
         time.sleep(FOLLOW_SLEEP)
         
         follow_buttons = self.driver.find_element(By.XPATH, "/html/body/div[6]/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[2]").find_elements(By.TAG_NAME, "button")
@@ -63,4 +82,4 @@ class InstaFollower:
         for button in follow_buttons:
             self.driver.execute_script("arguments[0].scrollIntoView();", button)
             button.click()
-            
+            time.sleep(FOLLOW_SLEEP)
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import os
+import time
+from dotenv import load_dotenv
 from insta_follower import InstaFollower
 
+# link to account
+ACCOUNT_TO_FOLLOW = os.environ.get("ACCOUNT_TO_FOLLOW")
 
+# creates InstaFollower object. logs into Instagram. Finds followers for given account. Follows everyone in account
 follower = InstaFollower()
 follower.login()
-follower.find_followers()
-follower.follow()
+follower.find_followers(link=ACCOUNT_TO_FOLLOW)
